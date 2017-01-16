d2dcaf0be0d2fd0b989796ac69e574fb4608bca3
webmaxxSELF <webmxxself@163.com>
2017-01-16 19:08:43 +0800

    ball

--- canvas-ball.js
+++ canvas-ball.js
@@ -7,36 +7,39 @@
 
     ctx.ary=[];// 保存数组 存入实例
     function Ball(opt){
-        this.r=30;
+        this.r=opt.r||30;
         this.x=opt.x;
         this.y=opt.y;
         this.cX= this.cY= Math.random()*10-5; // -5  5
+        this.cR=Math.random()+0.3; // 0.3 -1.3
+        this.color='rgb('+ parseInt(Math.random()*20) +','+ parseInt(Math.random()*255)+','+ parseInt(Math.random()*255)+')';
         ctx.ary.push(this);
     };
     Ball.prototype={
         update:function(){
-            this.r--;
+            this.r-=this.cR;
             this.x+=this.cX;
             this.y+=this.cY;
             return this;
         },
         render: function () {
             if(this.r<=0) {
-
+                ctx.ary.splice(ctx.ary.indexOf(this),1);
                 return;
-            }; //半径变小于0时  不再变化
+            }; //半径变小于0时  不再变化 并且在数组中删掉该实例
 
             ctx.beginPath();
             ctx.arc(this.x,this.y,this.r,0,Math.PI*2,false);
-            ctx.fillStyle='#f1f1f1';
+            ctx.fillStyle=this.color;
             ctx.fill();
+            ctx.closePath();
         }
     };
 
 
     //var ball = new Ball({x:60,y:200});
     // 开启定时器 小球变小
-    timer=setInterval( balling ,30);
+    timer=setInterval( balling ,80);
     function balling(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         for(var i=0;i<ctx.ary.length;i++){
