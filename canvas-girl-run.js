5a023659ea0f89e4edc9ac754ea45d95e0d43e11
webmaxxSELF <webmxxself@163.com>
2017-01-16 13:15:32 +0800

    gun

--- canvas-girl-run.js
+++ canvas-girl-run.js
@@ -24,21 +24,19 @@
     function run() {
         ++index;
             ctx.clearRect(0,0,canvas.width,canvas.height); // 清除画布
-        //ctx.drawImage(img,96*(index%4),96*2,96,96,10,10,96*2,96*2); //角色原地跑
-
         // 判断是否达到边缘
         if( ctx.flag == 'right'){ // 向右跑
             ctx.x+=30;
             if(ctx.x>500) {
                 ctx.flag = 'left';
             }
-            ctx.drawImage(img,96*(index%4),96*2,96,96,ctx.x,10,96*2,96*2); //加入横向跑
+            ctx.drawImage(img,96*(index%4),96*2,96,96,ctx.x,10,96*2,96*2); //图片位置
         }else  if( ctx.flag == 'left'){ // 向左跑
             ctx.x-=30;
             if(ctx.x<100){
                 ctx.flag = 'right';
             }
-            ctx.drawImage(img,96*(index%4),96,96,96,ctx.x,10,96*2,96*2); //加入横向跑
+            ctx.drawImage(img,96*(index%4),96,96,96,ctx.x,10,96*2,96*2); //图片位置
         }
 
     };
